140505643ee2a7a6aa629d1ce51ffa6d57b7a050
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-07-02 13:16:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "accurics-foreach-example-bucket" {
+  acl = "log-delivery-write"
+  for_each = toset( ["accurics-foreach-example-bucket1", "accurics-foreach-example-bucket2", "accurics-foreach-example-bucket3"] )
+  bucket     = each.key
+
+  tags = {
+    Tool    = var.tags-tool
+    Name    = each.key
+  }
+}
+
+
+resource "aws_s3_bucket" "accurics-foreach-index-bucket" {
+  acl = "log-delivery-write"
+  count = length(var.bucket_names)
+  bucket  = var.bucket_names[count.index]
+  tags = {
+    Tool    = var.tags-tool
+    Name    = var.bucket_names[count.index]
+  }
+}
